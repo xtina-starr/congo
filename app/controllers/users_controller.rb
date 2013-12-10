080e2877a623076335f569e261ca75c19d71ad20
Melinda Mizuta <mkmizuta@gmail.com>
2013-12-10 14:13:47 -0800

    User CRUD working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,57 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def index
-    # @users = User.all
+    @users = User.all
   end
+
+  def show
+    @users = User.all
+  end
+
+  def new
+    @users = User.new
+  end
+
+  def create
+    @users = User.new(user_params)
+
+    respond_to do |format|
+      if @users.save
+        format.html { redirect_to @users, notice: 'User was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @users.update(user_params)
+        format.html { redirect_to @users, notice: 'User was successfully updated.' }
+      else
+        format.html { render action: 'edit' }
+      end
+    end
+  end
+
+  def destroy
+    @users.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @users = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
+
+
