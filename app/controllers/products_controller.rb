759e4e09f1b059a18dd7eafca74a5e48d7588a36
Simone <msrondys@gmail.com>
2013-12-10 12:07:22 -0800

    C.R for products done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,57 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  # GET /products
+
   def index
   end
 
+  # GET /products/1
   def show
-    erb :products
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+    
+  end
+
+  # POST /products
+  def create
+    
+    @product = Product.new(product_params)
+
+      respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'product was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @product }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    
+  end
+
+  # DELETE /products/1
+  def destroy
+    
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price => {}) 
   end
 end
