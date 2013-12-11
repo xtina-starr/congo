4bdee9fcec43aa455147b39f6be9a136e0c026d4
liz rush <liz@lizmrush.com>
2013-12-11 12:04:53 -0800

    adds product search bar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
 
 
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
