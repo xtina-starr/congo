327786104d349f56987eeffa0823ebd74b4c8a99
Simone <msrondys@gmail.com>
2013-12-12 08:57:00 -0800

    added user relationship rspec tests, now passing; created user model body nil rspec test for team member

--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -10,5 +10,21 @@ describe User do
     it "must have an email" do
       expect(User.new(:email => nil)).to be_invalid
     end
+
+    it "with a nil body had an error message" do
+      post = Post.new
+      post.valid?
+      expect(post.errors[:Body]).to include "Can't be blank"
+    end
+
+    it "should have many products" do
+      t = User.reflect_on_association(:products)
+      t.macro.should == :has_many
+    end
+
+    it "should have many orders" do
+      t = User.reflect_on_association(:orders)
+      t.macro.should == :has_many
+    end
   end
 end
