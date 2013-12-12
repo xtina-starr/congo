b090ed8570df8dea44368c80db4c5a57587271d0
Simone <msrondys@gmail.com>
2013-12-12 13:51:07 -0800

    user model validations and rspec done, sans password_confirmation rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
-  validates :name, :email, presence: true
-  validates :name, :email, uniqueness: true
+# attr_accessible :name, :email, :password, :password_confirmation
 
+# Associations 
   has_many :products
   has_many :orders
+
+# Validations
+  validates :name,         presence: true,
+                           uniqueness: true
+  
+  validates :email,        presence: true,
+                           uniqueness: true   
+
+  validates :password,     presence: true,
+                           confirmation: true
+  
+  validates :password_confirmation,   presence: true
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131211190307) do
+ActiveRecord::Schema.define(version: 20131212204215) do
 
   create_table "order_items", force: true do |t|
     t.datetime "created_at"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20131211190307) do
     t.boolean  "merchant"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password"
   end
 
 end
