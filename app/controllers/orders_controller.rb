e60ce6bfb97a129bccc8654a57a92c3a2999d725
liz rush <liz@lizmrush.com>
2013-12-17 13:56:55 -0800

    cart shows items, heck yeah

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   before_action :set_user, only: [:show] 
+  before_action :find_cart
     #:update, :destroy add later so that the user can cancel or update order before it ships
     
     def index
@@ -12,14 +13,35 @@ class OrdersController < ApplicationController
     end
 
     def cart
-     @order = Order.pending
+     
     end
 
 # endpoint for "buy me" button on product page
     def add_to_cart
       # @order = Order.new
 
-      # begin find_cart
+ 
+
+      # add_item_to_order(order = @order) EXAMPLE SAMPLE
+      @order_item = OrderItem.new
+      @order_item.product_id = params[:product]
+      @order_item.order_id   = @order.id
+      @order_item.quantity   = params[:order_item][:quantity] || 1
+      @order_item.save
+
+      redirect_to product_path(params[:product])
+    end
+
+
+    def update_cart
+
+    
+    end
+
+  private
+
+def find_cart
+     # begin find_cart
       if session[:order_id]
         @order = Order.find(session[:order_id])
       else
@@ -36,56 +58,6 @@ class OrdersController < ApplicationController
 
       session[:order_id] = @order.id
       # end find_cart
-
-      # add_item_to_order(order = @order) EXAMPLE SAMPLE
-      @order_item = OrderItem.new
-      @order_item.product_id = params[:product]
-      @order_item.order_id   = @order.id
-      @order_item.quantity   = params[:order_item][:quantity] || 1
-      @order_item.save
-
-      redirect_to product_path(params[:product])
     end
-
-      # if Order.pending
-      #   @order = Order.pending
-      # else
-      #   @order = Order.new
-
-# update cart amount
-    # def update_cart
-    #   @order = Order.pending
-    #   @order.add_items_to_cart(params[:orders][:products])
-    # end
-  
-
-  # order.order_items.each |orderitem|
-  # order.product_id
-  # order_items.product.name
-  # order_items.quantity
-
-  # .order_items gets specific info like quantity and discount
-
-  # .oi.quantity
-
-  # invoice (order)
-  # line items (order items)
-
-
-  private
-
-  def add_items_to_cart(product_id)
-    order.order_items << Product.find(product_id)
-      # p = Product.find(product_id)
-      # o = OrderItem.new 
-      # o.product = p
-  end
 end
-
-# find a product by the product_id
-# make that product an order item
-# put that order item into cart 
-
-
-
 
