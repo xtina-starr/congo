7aa0bc1cf6a25c0890a2bf816801e1ee7fcb8b58
Melinda Mizuta <mkmizuta@gmail.com>
2013-12-17 10:45:52 -0800

    edited order table with status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216185523) do
+ActiveRecord::Schema.define(version: 20131217184210) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -30,9 +30,8 @@ ActiveRecord::Schema.define(version: 20131216185523) do
   create_table "orders", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "pending"
-    t.boolean  "shipped"
-    t.boolean  "cancelled"
+    t.string   "status"
+    t.integer  "user_id"
   end
 
   create_table "products", force: true do |t|
