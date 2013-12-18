e096ecac67d85d65ce7f5e18c59a7754966a4d2a
Simone <msrondys@gmail.com>
2013-12-18 13:38:43 -0800

    changed schema for retired products

--- db/migrate/20131217223243_add_retired_to_products.rb
+++ db/migrate/20131217223243_add_retired_to_products.rb
@@ -1,5 +1,5 @@
 class AddRetiredToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :retired, :boolean, default: false
+    add_column :products, :retired, :boolean, default: true
   end
 end
