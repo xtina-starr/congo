0a098f0388a9cbbadde72e532f2c4cef4b03a1de
Simone <msrondys@gmail.com>
2013-12-18 13:52:32 -0800

    updated booleans for retired products

--- db/migrate/20131217223243_add_retired_to_products.rb
+++ db/migrate/20131217223243_add_retired_to_products.rb
@@ -1,5 +1,5 @@
 class AddRetiredToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :retired, :boolean, default: true
+    add_column :products, :retired, :boolean, default: false
   end
 end
