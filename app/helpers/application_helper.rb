430a58de2f0c097ff71071d02267b4fa4e035f30
Sue White <sue@mailworks.org>
2013-12-19 13:44:44 -0800

    working users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
 
   def signed_in?
-    STDERR.puts "cookies[:user_id] = #{cookies[:user_id]}"
     if cookies[:user_id] != nil
-      STDERR.puts "User's database token    =  #{User.find(cookies[:user_id].to_i).remember_token}"
-      STDERR.puts "cookies[:remember_token] = #{cookies[:remember_token]}"
     end
     cookies[:user_id] != nil && User.find(cookies[:user_id].to_i).remember_token == cookies[:remember_token]
   end
@@ -19,9 +16,6 @@ module ApplicationHelper
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     cookies.permanent[:user_id] = user.id
-    STDERR.puts "sign_in:"
-    STDERR.puts "New cookie: user_id=#{user.id}"
-    STDERR.puts "New cookie: remember_token=#{remember_token}"
     user.update_attribute(:remember_token, remember_token)
   end
 end
