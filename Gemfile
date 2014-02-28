2b126b4c015fc8b3c69c2fb9835986eff47e2ea2
Galina <yermichevagalina@gmail.com>
2014-02-28 15:16:27 -0800

    added js to display subregions of selected country only

--- Gemfile
+++ Gemfile
@@ -1,30 +1,10 @@
 source 'https://rubygems.org'
-
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
-
-# Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
-
-# Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-
-# Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
-
-# See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
-
-# Use jquery as the JavaScript library
 gem 'jquery-rails'
-
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
-
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
-
-# Password Encryption
 gem 'bcrypt-ruby', :require => 'bcrypt'
 
 gem 'better_errors'
@@ -41,7 +21,6 @@ group :development do
 end
 
 group :doc do
-  # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
 end
 
@@ -54,14 +33,3 @@ end
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'carmen-rails','~> 1.0.0'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.1.2'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,92 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+
+  var  us = '<select id="order_state" name="order[state]"><option value="AK">AK</option>' +
+'<option value="AL">AL</option>'+
+'<option value="AR">AR</option>'+
+'<option value="AS">AS</option>'+
+'<option value="AZ">AZ</option>'+
+'<option value="CA">CA</option>'+
+'<option value="CO">CO</option>'+
+'<option value="CT">CT</option>'+
+'<option value="DC">DC</option>'+
+'<option value="DE">DE</option>'+
+'<option value="FL">FL</option>'+
+'<option value="GA">GA</option>'+
+'<option value="GU">GU</option>'+
+'<option value="HI">HI</option>'+
+'<option value="IA">IA</option>'+
+'<option value="ID">ID</option>'+
+'<option value="IL">IL</option>'+
+'<option value="IN">IN</option>'+
+'<option value="KS">KS</option>'+
+'<option value="KY">KY</option>'+
+'<option value="LA">LA</option>'+
+'<option value="MA">MA</option>'+
+'<option value="MD">MD</option>'+
+'<option value="ME">ME</option>'+
+'<option value="MI">MI</option>'+
+'<option value="MN">MN</option>'+
+'<option value="MO">MO</option>'+
+'<option value="MP">MP</option>'+
+'<option value="MS">MS</option>'+
+'<option value="MT">MT</option>'+
+'<option value="NC">NC</option>'+
+'<option value="ND">ND</option>'+
+'<option value="NE">NE</option>'+
+'<option value="NH">NH</option>'+
+'<option value="NJ">NJ</option>'+
+'<option value="NM">NM</option>'+
+'<option value="NV">NV</option>'+
+'<option value="NY">NY</option>'+
+'<option value="OH">OH</option>'+
+'<option value="OK">OK</option>'+
+'<option value="OR">OR</option>'+
+'<option value="PA">PA</option>'+
+'<option value="PR">PR</option>'+
+'<option value="RI">RI</option>'+
+'<option value="SC">SC</option>'+
+'<option value="SD">SD</option>'+
+'<option value="TN">TN</option>'+
+'<option value="TX">TX</option>'+
+'<option value="UM">UM</option>'+
+'<option value="UT">UT</option>'+
+'<option value="VA">VA</option>'+
+'<option value="VI">VI</option>'+
+'<option value="VT">VT</option>'+
+'<option value="WA">WA</option>'+
+'<option value="WI">WI</option>'+
+'<option value="WV">WV</option>'+
+'<option value="WY">WY</option></select>';
+
+var ca = '<select id="order_state" name="order[state]"><option value="AB">AB</option>'+
+'<option value="BC">BC</option>'+
+'<option value="MB">MB</option>'+
+'<option value="NB">NB</option>'+
+'<option value="NL">NL</option>'+
+'<option value="NS">NS</option>'+
+'<option value="NT">NT</option>'+
+'<option value="NU">NU</option>'+
+'<option value="ON">ON</option>'+
+'<option value="PE">PE</option>'+
+'<option value="QC">QC</option>'+
+'<option value="SK">SK</option>'+
+'<option value="YT">YT</option></select>';
+
+  $("#order_country").on("change", "", function() {
+ var sop = $('#states_or_provinces');
+
+    sop.find('select').remove();
+
+    if (this.value === 'CA') {
+      sop.append(ca);
+    }
+    else {
+      sop.append(us);
+    }
+  });
+});
