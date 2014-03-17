b5c11e4147fed002affad54196aed06e2e809d07
Galina <yermichevagalina@gmail.com>
2014-03-16 18:36:35 -0700

    merge conflict fixed

--- spec/models/order_item_model_spec.rb
+++ spec/models/order_item_model_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
-describe OrderItem do 
+describe OrderItem do
 
   describe "validations" do
 
     it "must belong to a product" do
       expect(OrderItem.new(:product_id => nil)).to be_invalid
     end
-  
+
     it "must belong to an order" do
-      expect(OrderItem.new(:order => nil)).to be_invalid
+      expect(OrderItem.new(:order_id => nil)).to be_invalid
     end
 
-# Basic Attribute Validations 
+# Basic Attribute Validations
 
     it "must have a quantity" do
+
       expect(OrderItem.new(:quantity => nil)).to be_invalid
+      puts OrderItem
     end
   end
 end
